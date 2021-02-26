5053c7b1ddce428f9d2fb9ee3e809133e49748d8
tylermachado <3586466+tylermachado@users.noreply.github.com>
2021-02-25 21:40:51 -0500

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Fresh Start
-      </header>
+      <section className="controller">
+        matter .. antimatter
+      </section>
+      <section className="area-quarks zone">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </section>
+      <section className="area-neutrinos zone">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </section>
+      <section className="area-force zone">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </section>
+      <section className="area-higgs-boson zone">
+        <div></div>
+      </section>
+      <section className="infobox">
+        <h3>Particle</h3>
+        <p>Dynamic info goes here</p>
+      </section>
     </div>
   );
 }
