482083452a79fbc94ceb5c2f23d39ace168b1664
tylermachado <3586466+tylermachado@users.noreply.github.com>
2021-03-14 18:36:39 -0400

    designing for greek symbols

--- src/App.js
+++ src/App.js
@@ -29,32 +29,32 @@ class App extends Component {
           matter .. antimatter
         </section>
         <section className="area-higgs-boson particle" onMouseOver={() => this.changeActive("higgs boson")}>
-          Hb
+          H
         </section>
         <div className="grid-container">
           <section className="area-quarks zone" onMouseOver={() => this.changeActive("quarks")}>
-            <div class="particle"></div>
-            <div class="particle"></div>
-            <div class="particle"></div>
-            <div class="particle"></div>
-            <div class="particle"></div>
-            <div class="particle"></div>
+            <div class="particle">u</div>
+            <div class="particle">c</div>
+            <div class="particle">t</div>
+            <div class="particle">d</div>
+            <div class="particle">s</div>
+            <div class="particle">b</div>
           </section>
           <section className="area-charged-leptons zone" onMouseOver={() => this.changeActive("charged leptons")}>
-            <div class="particle"></div>
-            <div class="particle"></div>
-            <div class="particle"></div>
+            <div class="particle">e</div>
+            <div class="particle">μ</div>
+            <div class="particle">τ</div>
           </section>
           <section className="area-neutrinos zone" onMouseOver={() => this.changeActive("neutrinos")}>
-            <div class="particle"></div>
-            <div class="particle"></div>
-            <div class="particle"></div>
+            <div class="particle">ν<sub>e</sub></div>
+            <div class="particle">ν<sub>μ</sub></div>
+            <div class="particle">ν<sub>τ</sub></div>
           </section>
           <section className="area-force-carrying-particles zone" onMouseOver={() => this.changeActive("force-carrying particles")}>
-            <div class="particle"></div>
-            <div class="particle"></div>
-            <div class="particle"></div>
-            <div class="particle"></div>
+            <div class="particle">γ</div>
+            <div class="particle">g</div>
+            <div class="particle">W+</div>
+            <div class="particle">Z</div>
           </section>
           <section className="infobox">
             <h3>{this.state.active.type}</h3>
